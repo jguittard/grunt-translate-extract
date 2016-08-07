10ccaf15c9ea151d3f52df520f9387393c84ac18
Julien Guittard <julien.guittard@me.com>
2016-08-06 22:21:04 -0400

    Fixed relPath type in Options

--- libs/tasks/Options.js
+++ libs/tasks/Options.js
@@ -1,7 +1,7 @@
 var path = require("path");
 var Options = (function () {
     function Options(relPath) {
-        if (relPath === void 0) { relPath = ""; }
+        if (relPath === undefined) { relPath = ""; }
         this.output = ['en.json', 'es.json', 'fr.json', 'de.json'];
         this.outputDir = "locales";
         this.basePath = null;
